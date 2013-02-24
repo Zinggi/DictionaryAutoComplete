fa46acd7f897a4a775e175897ade0cc83233e389
Zinggi <schrott57bla@gmail.com>
2013-02-24 23:13:13 +0100

    First and probably last version
    nothing to say here...

---
+++ DictionaryAutoComplete.py
@@ -0,0 +1,59 @@
+#-----------------------------------------------------------------------------------
+# Dictionary Auto-Complete
+#-----------------------------------------------------------------------------------
+#
+# This plug-in adds auto-completion entries from the dictionary file.
+# useful for very lazy typers or if you're searching for a particular word.
+#
+# (c) Florian Zinggeler
+#-----------------------------------------------------------------------------------
+import sublime
+import sublime_plugin
+import os
+
+
+class DictionaryAutoComplete(sublime_plugin.EventListener):
+    settings = None
+    b_first_edit = True
+    word_list = []
+
+    # on first modification in comments, get the dictionary and save it.
+    def on_modified(self, view):
+        if self.b_first_edit:
+            scope_name = sublime.windows()[0].active_view().scope_name(sublime.windows()[0].active_view().sel()[0].begin())
+            if "comment" in scope_name or "string.quoted" in scope_name:
+                if not self.settings:
+                    self.settings = sublime.load_settings('Preferences.sublime-settings')
+
+                    self.dict_path = os.path.join(sublime.packages_path()[:-9], self.settings.get('dictionary'))
+                    self.dict_path = self.dict_path.replace("/", "\\")
+                    print self.dict_path
+                    with open(self.dict_path, 'r') as dictionary:
+                        words = dictionary.read().split('\n')
+                        for word in words:
+                            word = word.split('/')[0]
+                            self.word_list.append(word)
+                self.b_first_edit = False
+
+    # This will return all words found in the dictionary.
+    def get_autocomplete_list(self, word):
+        autocomplete_list = []
+
+        # filter relevant items:
+        for w in self.word_list:
+            try:
+                if word.lower() in w.lower():
+                    autocomplete_list.append((w, w))
+            except UnicodeDecodeError:
+                continue
+
+        return autocomplete_list
+
+    # gets called when auto-completion pops up.
+    def on_query_completions(self, view, prefix, locations):
+        completions = []
+        scope_name = sublime.windows()[0].active_view().scope_name(sublime.windows()[0].active_view().sel()[0].begin())
+        if "comment" in scope_name or "string.quoted" in scope_name:
+            return self.get_autocomplete_list(prefix)
+
+        return (completions, sublime.INHIBIT_EXPLICIT_COMPLETIONS)
---
+++ README.md
@@ -0,0 +1,47 @@
+Dictionary Auto-Complete
+========================
+
+![Pic](http://www.mediafire.com/conv/192f62de03bd6fe2c26294faa932583c1a41bf47b48c96dd9dd41a9ac925277d6g.jpg)
+
+This plug-in adds auto-completion entries from the dictionary file if you are typing inside comments or in strings.
+Useful for very lazy typers or if you're searching for a particular word.
+
+In a comment or string, just type Ctrl + Space to show auto-completion.
+
+
+Alternatively, you can add this entry to your 'Settings - User' to always show auto-completion, regardless where you're typing:
+<pre>"auto_complete_selector": "source"</pre>
+
+
+Installation
+------------
+**Very easy with [Package Control](http://wbond.net/sublime_packages/package_control) right inside Sublime Text 2 (Package Control needs to be installed):**
+
+1.	Ctrl + shift + P
+2.  Search for "inst", hit enter
+3.  Search for "DictionaryAutoComplete", hit enter
+
+**Manually (not recommended):**
+
+1.  Clone or download this package
+2.	Put it into your Packages directory (find using 'Preferences' -> 'Browse Packages...')
+
+
+* * *
+License
+------------
+Dictionary Auto-Complete for Sublime Text 2
+Copyright (C) 2013 Florian Zinggeler
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.

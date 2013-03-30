b14276f63f0a2d602395dca1262143978763a6ab
Zinggi <schrott57bla@gmail.com>
2013-03-30 11:49:16 +0100

    fixed slow down at sturtup

--- DictionaryAutoComplete.py
+++ DictionaryAutoComplete.py
@@ -15,25 +15,32 @@ import os
 class DictionaryAutoComplete(sublime_plugin.EventListener):
     settings = None
     b_first_edit = True
+    b_fully_loaded = True
     word_list = []
 
     # on first modification in comments, get the dictionary and save it.
     def on_modified(self, view):
-        if self.b_first_edit:
-            scope_name = sublime.windows()[0].active_view().scope_name(sublime.windows()[0].active_view().sel()[0].begin())
-            if "comment" in scope_name or "string.quoted" in scope_name:
-                if not self.settings:
-                    self.settings = sublime.load_settings('Preferences.sublime-settings')
+        if self.b_first_edit and self.b_fully_loaded:
+            self.b_fully_loaded = False
+            sublime.set_timeout(lambda: self.load_completions(view), 3)
 
-                    self.dict_path = os.path.join(sublime.packages_path()[:-9], self.settings.get('dictionary'))
-                    self.dict_path = self.dict_path.replace("/", "\\")
-                    print self.dict_path
-                    with open(self.dict_path, 'r') as dictionary:
-                        words = dictionary.read().split('\n')
-                        for word in words:
-                            word = word.split('/')[0]
-                            self.word_list.append(word)
+    def load_completions(self, view):
+        scope_name = view.scope_name(view.sel()[0].begin())       # sublime.windows()[0].active_view()
+        if "comment" in scope_name or "string.quoted" in scope_name:
+            if not self.settings:
+                self.settings = sublime.load_settings('Preferences.sublime-settings')
+
+                self.dict_path = os.path.join(sublime.packages_path()[:-9], self.settings.get('dictionary'))
+                self.dict_path = self.dict_path.replace("/", "\\")
+                print self.dict_path
+                with open(self.dict_path, 'r') as dictionary:
+                    words = dictionary.read().split('\n')
+                    for word in words:
+                        word = word.split('/')[0]
+                        self.word_list.append(word)
                 self.b_first_edit = False
+        else:
+            self.b_fully_loaded = True
 
     # This will return all words found in the dictionary.
     def get_autocomplete_list(self, word):

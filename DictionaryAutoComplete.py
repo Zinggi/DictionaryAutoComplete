3fb0203c063146c7ee017fc58c5813f0455242fe
Kroum Tzanev <kpym.tzanev@gmail.com>
2018-10-25 07:39:47 +0200

    comment the code and better managment of insert_original setting

--- DictionaryAutoComplete.py
+++ DictionaryAutoComplete.py
@@ -18,15 +18,22 @@ if not ST3:
     from codecs import open
 
 def get_setting(lang=None):
-    global dict_encoding, insert_original, max_results, scopes, minimal_len, forbidden_prefixes, local_dictionary
+    """
+    Read all the settings from previously initialized global settings.
+    This function is called when the language is changed (or initialized).
+    All the setting variables are global (for this module).
+    """
+    global dict_encoding, insert_original, max_results, allowed_scopes, minimal_len, forbidden_prefixes, local_dictionary
     print("[DictionaryAutoComplete] Get parameters for ",lang)
+    # set the default values (language independent)
     dict_encoding = settings.get('encoding', 'UTF-8')
     insert_original = settings.get('insert original', False)
     max_results = int(settings.get('maximum results', 1000))
-    scopes = settings.get('scopes', ["comment", "string.quoted", "text"])
+    allowed_scopes = settings.get('scopes', ["comment", "string.quoted", "text"])
     minimal_len = max(1,settings.get('minimal length',1)) # never fire on zero length
     forbidden_prefixes = settings.get('forbidden prefixes',[])
     local_dictionary = settings.get('dictionary', None)
+    # set the language independent values
     if lang:
         languages = settings.get("languages",{})
         if lang in languages:
@@ -34,12 +41,16 @@ def get_setting(lang=None):
             dict_encoding = local_settings.get('encoding', dict_encoding)
             insert_original = local_settings.get('insert original', insert_original)
             max_results = local_settings.get('maximum results', max_results)
-            scopes = local_settings.get('maximum results', scopes)
+            allowed_scopes = local_settings.get('maximum results', allowed_scopes)
             minimal_len = local_settings.get('minimal length', minimal_len)
             forbidden_prefixes = local_settings.get('forbidden prefixes', forbidden_prefixes)
             local_dictionary = local_settings.get('dictionary', local_dictionary)
 
 def plugin_loaded():
+    """
+    This method is (automatically in ST3) loaded when the plug-in is ready.
+    It reads the global variable `settings` from 'DictionaryAutoComplete.sublime-settings'.
+    """
     global settings
     print('[DictionaryAutoComplete] plug-in is loaded.')
     # declare the settings parameters as global variables
@@ -48,19 +59,29 @@ def plugin_loaded():
     get_setting()
 
 class DictionaryAutoComplete(sublime_plugin.EventListener):
-    request_load = True
-    last_language = ""
-    word_dict_list = {}
+    request_load = True # used to avoid multiple calls of on_activated_async
+    last_language = "" # used to optimize the dictionary load in load_completions
+    word_dict_list = {} # the entire dictionary as {"pref" : ["prefix", "Prefab", ...], ...}
 
-    # on first activation of the view, call load_completions asynchronously
     def on_activated_async(self, view):
+        """
+        Called by ST on the first activation of the view.
+        It calls load_completions asynchronously.
+        """
         if self.request_load:
             self.request_load = False
             sublime.set_timeout(lambda: self.load_completions(view), 3)
             view.settings().add_on_change('dictionary', lambda: self.load_completions(view))
 
-    # create the word_list containing all the words of the dictionary
     def load_completions(self, view):
+        """
+        Create the word_dict_list containing all the words of the dictionary.
+        The format of word_dict_list is
+            {"pref" : ["prefix", "Prefab", ...], ...}
+        where the length of "pref" is determined by `minimal_len` global setting variable.
+        This method is called on the first activation of the view and when the dictionary (language) is changed.
+        If this method is called without a language change it simply returns.
+        """
         dictionary = view.settings().get('dictionary')
         if not dictionary:
             return
@@ -83,9 +104,13 @@ class DictionaryAutoComplete(sublime_plugin.EventListener):
             except Exception as e:
                 print('[DictionaryAutoComplete] Error reading from dictionary : ' + e)
 
-            # optimise the list
-            del words[0:1]
+            # optimize the list
+            # the first line of .dic file is the number of words
+            if not local_dictionary:
+                del words[0:1]
+            # keep only words longer than the minimal prefix length
             words = [word for word in words if len(word) >= minimal_len]
+
             # create dictionary of prefix -> list of words
             self.word_dict_list = {}
             for word in words:
@@ -100,6 +125,14 @@ class DictionaryAutoComplete(sublime_plugin.EventListener):
 
     # This will return all words found in the dictionary.
     def get_autocomplete_list(self, view, prefix):
+        """
+        Returns the auto-completion list.
+        In general it is called when the prefix has the minimal length.
+        And then it is called when ST consider that it is appropriate.
+        In general it is not called on every key press.
+        """
+        autocomplete_list = [] # the list of tuples to return
+
         # prepare the prefix to search for
         if prefix.istitle():
             def correctCase(x): return x[:1].upper()+x[1:] if x.islower() else x
@@ -107,30 +140,33 @@ class DictionaryAutoComplete(sublime_plugin.EventListener):
             def correctCase(x): return x.upper() if x.islower() else x
         else:
             def correctCase(x): return x
-        prefix = prefix.lower()
+        prefix_lower = prefix.lower()
+        prefix_length = len(prefix_lower)
+        pref = prefix_lower[:minimal_len] # a lower case prefix to look in the dictionary
+        suff = prefix_lower[minimal_len:] # a lower case suffix to look in the list of words
 
-        # filter relevant items:
-        index = 0
-        autocomplete_list = []
-        pref = prefix[:minimal_len] # a lower case prefix to look in the dictionary
-        suff = prefix[minimal_len:] # a lower case suffixe to look in the list of words
-        prefix_length = len(prefix)
-        if pref in self.word_dict_list:
-            for w in self.word_dict_list[pref]:
-                if minimal_len == prefix_length or w[minimal_len:prefix_length].lower() == suff:
-                    w = correctCase(w)
-                    autocomplete_list.append((w,w))
-                    index = index +1
-                    if index > max_results:
-                        break
+        # check the prefix length
+        if prefix_length >= minimal_len:
+            # filter relevant items
+            index = 0
+            if pref in self.word_dict_list:
+                for w in self.word_dict_list[pref]:
+                    if minimal_len == prefix_length or w[minimal_len:prefix_length].lower() == suff:
+                        w = correctCase(w)
+                        autocomplete_list.append((w,w))
+                        index = index +1
+                        if index > max_results:
+                            break
 
         # append the original auto-complete list ?
-        preventDefault = False # by default (or for example if insert_original=='default')
+        preventDefault = False # by default (if insert_original=='default')
         if insert_original == 'before':
-            autocomplete_list = [(w,w) for w in view.extract_completions(prefix)] + autocomplete_list
+            st_list = [(prefix+'\t'+w,w) for w in view.extract_completions(prefix) if w[:prefix_length].lower() == prefix_lower]
+            autocomplete_list =  st_list + autocomplete_list
             preventDefault = True
         elif insert_original == 'after':
-            autocomplete_list = autocomplete_list + [(w,w) for w in view.extract_completions(prefix)]
+            st_list = [(prefix+'\t'+w,w) for w in view.extract_completions(prefix) if w[:prefix_length].lower() == prefix_lower]
+            autocomplete_list = autocomplete_list + st_list
             preventDefault = True
         elif insert_original == 'none':
             preventDefault = True
@@ -140,25 +176,34 @@ class DictionaryAutoComplete(sublime_plugin.EventListener):
         return autocomplete_list
 
     def is_scope_ok(self, view, point):
-        # check if the cursor position is in allowed scope
-        for selector in scopes:
+        """
+        Check if the cursor position is in allowed_scopes (setting).
+        Called by on_query_completions to decide if it should call get_autocomplete_list.
+        Return True if the scope is allowed.
+        """
+        for selector in allowed_scopes:
             if view.match_selector(point, selector):
                 return True
         return False
 
     def is_forbidden_prefix(self, view, prefix, point):
+        """
+        Check if the character before the completion prefix is in the forbidden_prefixes (setting).
+        Called by on_query_completions to decide if it should call get_autocomplete_list.
+        Return True if the prefix follows forbidden character.
+        """
         # get the prefix character
         pos = point - len(prefix)
         ch = view.substr(sublime.Region(pos-1, pos))
-        # return true if it is forbidden
         return ch in forbidden_prefixes
 
 
-    # gets called when auto-completion pops up.
     def on_query_completions(self, view, prefix, locations):
-        # check the prefix length
-        if len(prefix) < minimal_len:
-            return None # Too short to complete
+        """
+        Called when ST needs an auto-completion.
+        In general it is not called on every key press but only when ST needs more items for list.
+        Returns the result of get_autocomplete_list if DictionaryAutoComplete is allowed in this place.
+        """
         # check if scope is allowed
         if not self.is_scope_ok(view, locations[0]):
             return None # Forbidden scope
